b5f22642516ee7e9e56ae70fd13cd65dfd739e82
GalacticSandwich <aarnicho02@gmail.com>
2023-09-07 16:30:49 -0400

    [Skeleton] Added placeholders for Boyer-Moore and Rabin-Karp implementations

---
+++ strsearch-rk.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "strings.h"
+#include "strsearch.h"
+
+int findSubstring( String const* str, String const* subs, int pos )
+{
+    /* ... */
+
+    return -1;
+}
