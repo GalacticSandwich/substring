0dabf7e2a877de23b57078744c674a4679032bff
GalacticSandwich <aarnicho02@gmail.com>
2023-09-07 17:16:28 -0400

    [Development] KMP Implementation Ready

--- substring.c
+++ substring.c
@@ -5,42 +5,59 @@
 #include "strings.h"
 #include "strsearch.h"
 
-int main( int argc, char** argv )
-{
-    int numArgs = argc;
-    char** argArr = argv;
+static char* readFile( char const* fileName ) {
+    // open up the source text file to read
+    FILE* file = fopen( fileName, "r" );
 
-    // open up the source text file
-    FILE* text = fopen( argArr[ 1 ], "r" );
-
-    if ( text == NULL ) {
+    if ( file == NULL ) {
         fprintf( stderr, "Error: Invalid file!\n" );
-        return EXIT_FAILURE;
+        exit( EXIT_FAILURE );
     }
 
     // initialize a value for the text-string length
     int n = 0;
 
     // initialize the file text byte array
-    char* rawData = ( char* ) malloc( sizeof( char ) );
+    char* data = ( char* ) malloc( sizeof( char ) );
 
     // read the file text into a character pointer
-    int c = fgetc( text );
+    int c = fgetc( file );
     while ( c != EOF ) {
-        rawData[ n++ ] = c;
-        rawData = ( char* ) realloc( rawData, n * sizeof( char ) + 1 );
-        c = fgetc( text );
+        data[ n++ ] = c;
+        data = ( char* ) realloc( data, n * sizeof( char ) + 1 );
+        c = fgetc( file );
     }
 
     // null-terminate the raw file text string
-    rawData[ n ] = '\0';
+    data[ n ] = '\0';
+
+    return data;
+}
+
+static void usage( char const* progName ) {
+    printf( "usage: %s <file> <string> <flags>\n", progName );
+}
+
+int main( int argc, char** argv )
+{
+    int numArgs = argc;
+    char** argArr = argv;
+
+    if ( numArgs < 3 ) {
+        usage( argArr[ 0 ] );
+        return EXIT_FAILURE;
+    }
+
+    // read in the input file
+    char* data = readFile( argArr[ 1 ] );
 
     // construct a string for the text and pattern using the created text string
     // and the word to search for passed in through the command line
-    String* haystack = makeString( rawData );
+    String* haystack = makeString( data );
     String* needle = makeString( argArr[ 2 ] );
-    free( rawData );
+    free( data );
 
+    // perform a substring search
     int pos = findSubstring( haystack, needle, 0 );
 
     // free the two strings made after we're done using them
