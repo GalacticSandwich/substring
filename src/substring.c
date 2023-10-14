60ab348915d9a329a1ddce500826cf3447b10061
GalacticSandwich <aarnicho02@gmail.com>
2023-10-14 14:36:39 -0400

    [Implementation] added iteration functionality

--- src/substring.c
+++ src/substring.c
@@ -28,11 +28,14 @@
                     indices at which they occur
 */
 typedef enum {
-    OP_NOTHING = 0x00,
-    OP_USAGE = 0x01,
-    OP_SEARCH = 0x02,
-    OP_SEARCHALL = 0x03
+    OP_NOTHING,
+    OP_USAGE,
+    OP_SEARCH,
+    OP_SEARCHALL
 } task_t;
+
+// the number of times to repeat a search
+long reps = 1;
 
 /**
     Reads a file using a provided filename into a heap-allocated string, then
@@ -79,8 +82,45 @@ static char* readFile( char const* fileName ) {
 static void usage( char const* progName ) {
     printf( "[substring] usage: \n" );
     printf( "\t%s usage\n", progName );
-    printf( "\t%s search <file> <string>\n", progName );
-    printf( "\t%s searchall <file> <string>\n", progName );
+    printf( "\t\t :: output program usages\n" );
+    printf( "\t%s <flags> search <file> <string>\n", progName );
+    printf( "\t\t :: search for the first instance of <string> in <file>\n" );
+    printf( "\t%s <flags> searchall <file> <string>\n", progName );
+    printf( "\t\t :: search for all instances of <string> in <file>\n" );
+    printf( "[substring] flags: \n" );
+    printf( "\t-i <reps> \t :: repeat an operation <reps> times.\n" );
+}
+
+static int evalFlags( int argc, char** argv ) {
+    int aup = 0;
+
+    for ( int i = 0; i < argc; i++ ) {
+        if ( argv[ i ][ 0 ] == '-' ) {
+            switch ( argv[ i ][ 1 ] ) {
+                case 'i':
+                    reps = strtol( argv[ i + 1 ], NULL, 10 );
+
+                    if ( PRINTING ) {
+                        printf( "[substring] flags: iteration value parsed - %ld\n", reps );
+                    }
+
+                    if ( reps < 0 ) {
+                        fprintf( stderr, "[substring] error: Invalid iteration value\n" );
+                        usage( argv[ 0 ] );
+                        exit( EXIT_FAILURE );
+                    }
+
+                    aup += 2;
+                    i++;
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    return aup;
 }
 
 /**
@@ -97,6 +137,9 @@ int main( int argc, char** argv )
     // the operation to perform by the driver program
     task_t operation = OP_NOTHING;
 
+    // evaluate the flags passed
+    int aup = evalFlags( argc, argv );
+
     if ( argc == 1 ) {
         fprintf( stderr, "[substring] error: No arguments provided\n" );
         usage( argv[ 0 ] );
@@ -104,8 +147,8 @@ int main( int argc, char** argv )
     }
 
 
-    if ( !strcmp( "search", argv[ 1 ] ) ) {
-        if ( argc != 4 ) {
+    if ( !strcmp( "search", argv[ aup + 1 ] ) ) {
+        if ( argc != ( aup + 4 ) ) {
             fprintf( stderr, "[substring] error: Insufficient arguments provided\n" );
             usage( argv[ 0 ] );
             exit( EXIT_FAILURE );
@@ -113,8 +156,8 @@ int main( int argc, char** argv )
 
         operation = OP_SEARCH;
     }
-    else if ( !strcmp( "searchall", argv[ 1 ] ) ) {
-        if ( argc != 4 ) {
+    else if ( !strcmp( "searchall", argv[ aup + 1 ] ) ) {
+        if ( argc != ( aup + 4 ) ) {
             fprintf( stderr, "[substring] error: Insufficient arguments provided\n" );
             usage( argv[ 0 ] );
             exit( EXIT_FAILURE );
@@ -122,8 +165,8 @@ int main( int argc, char** argv )
         
         operation = OP_SEARCHALL;
     }
-    else if ( !strcmp( "usage", argv[ 1 ] ) ) {
-        if ( argc != 2 ) {
+    else if ( !strcmp( "usage", argv[ aup + 1 ] ) ) {
+        if ( argc != ( aup + 2 ) ) {
             fprintf( stderr, "[substring] error: Redundant arguments provided\n" );
             usage( argv[ 0 ] );
             exit( EXIT_FAILURE );
@@ -132,7 +175,7 @@ int main( int argc, char** argv )
         operation = OP_USAGE;
     }
     else {
-        fprintf( stderr, "[substring] error: Invalid Command \"%s\"\n", argv[ 1 ] );
+        fprintf( stderr, "[substring] error: Invalid Command \"%s\"\n", argv[ aup + 1 ] );
         usage( argv[ 0 ] );
         exit( EXIT_FAILURE );
     }
@@ -147,53 +190,75 @@ int main( int argc, char** argv )
     }
     else {
         // read in the input file
-        char* data = readFile( argv[ 2 ] );
+        char* data = readFile( argv[ aup + 2 ] );
 
         // construct a string for the text and pattern using the created text string
         // and the word to search for passed in through the command line
         String* haystack = makeString( data );
-        String* needle = makeString( argv[ 3 ] );
+        String* needle = makeString( argv[ aup + 3 ] );
 
         // initialize the value table with appropriate algorithms that need to use it
         int varr[ needle->len ];
         preprocess( needle, varr );
 
-        // if the operation is not the usage display, check to see if the user has
-        // prompted a search or searchall
-        if ( operation == OP_SEARCH ) {
-            // perform a substring search
-            int pos = findSubstring( haystack, needle, 0, varr );
+        // perform however many iterations are passed to the driver
+        for ( long iter = 0; iter < reps; iter++ ) {
+            // if the operation is not the usage display, check to see if the user has
+            // prompted a search or searchall
+            if ( operation == OP_SEARCH ) {
+                // perform a substring search
+                int pos = findSubstring( haystack, needle, 0, varr );
 
-            // output the index of the first instance of the substring in the text read in, or
-            // an appropriate message if no such instance exists
-            if ( pos == -1 ) {
-                printf( "[substring] search: No substrings found in %s\n", argv[ 2 ] );
-            }
-            else {
-                printf( "[substring] search: Substring found in %s at index %d\n", argv[ 2 ], pos );
-            }
-        }
-        else if ( operation == OP_SEARCHALL ) {
-            // track the number of substrings found
-            int n = 0;
-            // perform a substring search
-            int pos = findSubstring( haystack, needle, 0, varr );
+                // output the index of the first instance of the substring in the text read in, or
+                // an appropriate message if no such instance exists
+                if ( pos == -1 ) {
+                    printf( "[substring] " );
+                    if ( reps > 1 )
+                        printf( "(%ld/%ld) ", iter + 1, reps );
 
-            // output an appropriate message if no such instance exists, otherwise
-            // output the correct index and keep searching until no more substrings can be found
-            if ( pos == -1 ) {
-                printf( "[substring] searchall: No substrings found in %s\n", argv[ 2 ] );
-            }
-            else {
-                // search for substrings if they exist
-                while ( pos != -1 ) {
-                    n++;
-                    printf( "[substring] searchall: Substring found in %s at index %d\n", argv[ 2 ], pos );
-                    pos = findSubstring( haystack, needle, pos + 1, varr );
+                    printf( "search: No substrings found in %s\n", argv[ aup + 2 ] );
+                }
+                else {
+                    printf( "[substring] " );
+                    if ( reps > 1 )
+                        printf( "(%ld/%ld) ", iter + 1, reps );
+
+                    printf( "search: Substring found in %s at index %d\n", argv[ aup + 2 ], pos );
                 }
             }
+            else if ( operation == OP_SEARCHALL ) {
+                // track the number of substrings found
+                int n = 0;
+                // perform a substring search
+                int pos = findSubstring( haystack, needle, 0, varr );
 
-            printf( "[substring] searchall: Total substrings found in %s: %d\n", argv[ 2 ], n );
+                // output an appropriate message if no such instance exists, otherwise
+                // output the correct index and keep searching until no more substrings can be found
+                if ( pos == -1 ) {
+                    printf( "[substring] " );
+                    if ( reps > 1 )
+                        printf( "(%ld/%ld) ", iter + 1, reps );
+
+                    printf( "searchall: No substrings found in %s\n", argv[ aup + 2 ] );
+                }
+                else {
+                    // search for substrings if they exist
+                    while ( pos != -1 ) {
+                        n++;
+                        printf( "[substring] " );
+                        if ( reps > 1 )
+                            printf( "(%ld/%ld) ", iter + 1, reps );
+
+                        printf( "searchall: Substring found in %s at index %d\n", argv[ aup + 2 ], pos );
+                        pos = findSubstring( haystack, needle, pos + 1, varr );
+                    }
+                }
+                printf( "[substring] " );
+                if ( reps > 1 )
+                    printf( "(%ld/%ld) ", iter + 1, reps );
+
+                printf( "searchall: Total substrings found in %s: %d\n", argv[ aup + 2 ], n );
+            }
         }
 
         // free the two strings made after we're done using them
